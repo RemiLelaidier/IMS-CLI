f1b874f070157efa70a1e2eca3071c79a6c5bd29
Léonard Cherouvrier <leonard@bigsool.com>
2018-05-15 21:54:37 +0200

    Validation fixes

--- src/components/steps/InternshipStep.tsx
+++ src/components/steps/InternshipStep.tsx
@@ -1,3 +1,4 @@
+import * as Joi from 'joi';
 import * as React from 'react';
 
 import './Step.css';
@@ -5,9 +6,10 @@ import './Step.css';
 import FormControl from '@material-ui/core/FormControl/FormControl';
 import FormGroup from '@material-ui/core/FormGroup/FormGroup';
 import FormLabel from '@material-ui/core/FormLabel/FormLabel';
+import Input from '@material-ui/core/Input/Input';
 import InputLabel from '@material-ui/core/InputLabel/InputLabel';
 import Select from '@material-ui/core/Select/Select';
-import TextField from '@material-ui/core/TextField/TextField';
+import { _handleField } from '../../validation/validation';
 
 interface InternshipProps {
     onError: (any);
@@ -15,79 +17,138 @@ interface InternshipProps {
 
 interface InternshipState {
     errors: {
-        // tofix
-        sexeRepresentant: boolean,
-        nomRepresentant: boolean,
-        prenomRepresentant: boolean,
-        emailRepresentant: boolean,
-        telephoneRepresentant: boolean,
-        qualiteRepresentant: boolean
+        intituleStage: boolean,
+        descriptionStage: boolean,
+        dateDebutStage: boolean,
+        dateFinStage: boolean,
+        dureeHebdoStage: boolean,
+        horairesStage: boolean,
+        presenceFree: boolean,
+        gratificationStage: boolean,
+        typeRemuneration: boolean,
+        avantagesStage: boolean
+    },
+    fields: {
+        intituleStage: string,
+        descriptionStage: string,
+        dateDebutStage: string,
+        dateFinStage: string,
+        dureeHebdoStage: string,
+        horairesStage: string,
+        presenceFree: string,
+        gratificationStage: string,
+        typeRemuneration: string,
+        avantagesStage: string
     }
 }
 export default class InternshipStep extends React.Component<InternshipProps, InternshipState> {
+    public schema: any;
+    private _handleChange: any;
+
+    constructor(props: InternshipProps){
+        super(props);
+        this.state = {
+            errors: {
+                intituleStage: false,
+                descriptionStage: false,
+                dateDebutStage: false,
+                dateFinStage: false,
+                dureeHebdoStage: false,
+                horairesStage: false,
+                presenceFree: false,
+                gratificationStage: false,
+                typeRemuneration: false,
+                avantagesStage: false
+            },
+            fields: {
+                intituleStage: '',
+                descriptionStage: '',
+                dateDebutStage: '',
+                dateFinStage: '',
+                dureeHebdoStage: '',
+                horairesStage: '',
+                presenceFree: '',
+                gratificationStage: '',
+                typeRemuneration: '',
+                avantagesStage: ''
+            }
+        }
+
+        this._handleChange = _handleField.bind(this);
+        this.schema = {
+            intituleStage: Joi.string().min(2),
+            descriptionStage: Joi.string().min(20),
+            dateDebutStage: Joi.string().regex(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/\-]\d{4}$/),
+            dateFinStage: Joi.string().regex(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/\-]\d{4}$/),
+            dureeHebdoStage: Joi.string().min(10),
+            horairesStage: Joi.string().min(10),
+            presenceFree: Joi.string().min(2),
+            gratificationStage: Joi.string().min(2),
+            typeRemuneration: Joi.string().min(2),
+            avantagesStage: Joi.string().min(2)
+        };
+    }
+
     public render() {
         return (
             <div>
                 <FormLabel component="legend">Informations sur le stage</FormLabel>
                 <br />
                 <FormGroup row={true}>
-                    <FormControl>
-                        <TextField 
-                            label="Intitulé du stage"
-                            className="internship_title"
+                    <FormControl required={true} error={this.state.errors.intituleStage}>
+                        <InputLabel htmlFor="intituleStage">Intitulé du stage</InputLabel>
+                        <Input 
+                            id="intituleStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
-                    <FormControl>
-                        <TextField 
-                            label="Description"
-                            className="internship_description"
+                    <FormControl required={true} error={this.state.errors.descriptionStage}>
+                        <InputLabel htmlFor="descriptionStage">Description</InputLabel>
+                        <Input 
+                            id="descriptionStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
                 </FormGroup>
                 <br />
                 <FormGroup row={true}>
-                    <FormControl>
-                        <TextField
-                            className="input-date"
-                            id="date"
-                            label="Date de début"
-                            type="date"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
+                    <FormControl required={true} error={this.state.errors.dateDebutStage}>
+                        <InputLabel htmlFor="dateDebutStage">Date de début</InputLabel>
+                        <Input
+                            id="dateDebutStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
-                    <FormControl>
-                        <TextField
-                            className="input-date"
-                            id="date"
-                            label="Date de fin"
-                            type="date"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
+                    <FormControl required={true} error={this.state.errors.dateFinStage}>
+                        <InputLabel htmlFor="dateFinStage">Date de fin</InputLabel>
+                        <Input
+                            id="dateFinStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
                 </FormGroup>
                 <FormGroup row={true}>
-                    <FormControl>
-                        <TextField 
-                            label="Durée hebdomadaire"
-                            className="duree-hebdo"
+                    <FormControl required={true} error={this.state.errors.dureeHebdoStage}>
+                        <InputLabel htmlFor="dureeHebdoStage">Durée hebdomadaire</InputLabel>
+                        <Input 
+                            id="dureeHebdoStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
-                    <FormControl>
-                        <TextField 
-                            label="Horaires"
-                            className="horaires_text"
+                    <FormControl required={true} error={this.state.errors.horairesStage}>
+                        <InputLabel htmlFor="horairesStage">Horaires</InputLabel>
+                        <Input 
+                            id="horairesStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
                 </FormGroup>
                 <FormGroup row={true}>
-                    <FormControl>
+                    <FormControl required={true} error={this.state.errors.presenceFree}>
                         <p>Présence la nuit, le dimanche et les jours fériés ?</p>
-                        <TextField 
-                            className="extra_horaires_text"
+                        <Input 
+                            id="presenceFree"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
                 </FormGroup>
@@ -95,20 +156,19 @@ export default class InternshipStep extends React.Component<InternshipProps, Int
               fait l'objet d'une gratification, et qui sera due à compter du premier jour du premier mois de stage et devra
               faire l'objet d'un versement mensuel.</p>
               <FormGroup row={true}>
-                <FormControl>
-                    <TextField 
-                        label="Gratification"
-                        className="internship_remuneration"
+                <FormControl required={true} error={this.state.errors.gratificationStage}>
+                    <InputLabel htmlFor="gratificationStage">Gratification</InputLabel>
+                    <Input 
+                        id="gratificationStage"
+                        onChange={this._handleChange}
                     />
                 </FormControl>
-                <FormControl>
-                    <InputLabel htmlFor="remuneration_way">Rémunération</InputLabel>
+                <FormControl required={true} error={this.state.errors.typeRemuneration}>
+                    <InputLabel htmlFor="typeRemuneration">Rémunération</InputLabel>
                     <Select
+                        id="typeRemuneration"
+                        onChange={this._handleChange}
                         native={true}
-                        className="input-text"
-                        inputProps={{
-                            id: 'remuneration_way',
-                        }}
                     >
                         <option>Virement</option>
                         <option>Chèque</option>
@@ -117,10 +177,11 @@ export default class InternshipStep extends React.Component<InternshipProps, Int
                  </FormControl>
                 </FormGroup>
                 <FormGroup row={true}>
-                    <FormControl>
-                        <TextField 
-                            label="Avantages fournis"
-                            className="internship_advantages"
+                    <FormControl required={true} error={this.state.errors.avantagesStage}>
+                        <InputLabel htmlFor="avantagesStage">Avantages fournis</InputLabel>
+                        <Input 
+                            id="avantagesStage"
+                            onChange={this._handleChange}
                         />
                     </FormControl>
                 </FormGroup>
