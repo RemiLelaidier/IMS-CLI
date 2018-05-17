652ef562b48e71759b26f7da0f358c9b11baf0e4
Léonard Cherouvrier <leonard@bigsool.com>
2018-05-17 21:58:21 +0200

    Bind admin panel to API

--- src/components/pages/AdminPage.tsx
+++ src/components/pages/AdminPage.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as React from 'react';
 
 import { Table, TableBody, TableCell, TableHead, TablePagination, TableRow, TextField } from '@material-ui/core';
@@ -5,14 +6,15 @@ import Paper from '@material-ui/core/Paper/Paper';
 import Typography from '@material-ui/core/Typography/Typography';
 
 interface AdminPageState {
-    page: number,
-    rowsPerPage: number
+    page: number;
+    rowsPerPage: number;
+    apiURL: string | undefined;
+    token: string | null;
+    data: any[]
 }
 
 // tslint:disable-next-line:no-empty-interface
 interface AdminPageProps { }
-
-let id = 0;
 
 export class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
     constructor(props: AdminPageProps) {
@@ -20,7 +22,10 @@ export class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
 
         this.state = {
             page: 0,
-            rowsPerPage: 25
+            rowsPerPage: 25,
+            apiURL: process.env.REACT_APP_API,
+            token: sessionStorage.getItem('imsToken'),
+            data: []
         }
 
         this._handleChangePage = this._handleChangePage.bind(this);
@@ -28,21 +33,27 @@ export class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
         this._handleSearch = this._handleSearch.bind(this);
     }
 
-    public render() {
-        const data = [
-            this.createData('Frozen yoghurt', 'Jean-Luc', 'En attente', 'xxx1'),
-            this.createData('Ice cream sandwich', 'Jean-Martin', 'En cours', 'xxx2'),
-            this.createData('Eclair', 'Jean-Michel', 'En attente', 'xxx3'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx4'),
-            this.createData('Gingerbread', 'Jean-Philippe', 'En cours', 'xxx5'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx6'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx7'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx8'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx9'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx10'),
-            this.createData('Cupcake', 'Jean-Louis', 'En attente', 'xxx11'),
-        ];
+    public async componentDidMount(){
+        const request = await axios.get(this.state.apiURL + 'conventions/getAll', {
+            headers: {
+                Authorization: this.state.token
+            }
+        });
+        
+        if(request.status === 200){
+            const conventions = request.data.data;
+            const rows: any[] = [];
+            conventions.forEach((convention: any, idx: number) => {
+                const row = this.createData(convention.entreprise.nomEntreprise, convention.etudiant.nom, '', convention.id);
+                rows.push(row);
+            });
+            this.setState({data:rows});
+        } else {
+            console.log('erf');
+        }
+    }
 
+    public render() {
         return (
             <Paper style={{ margin: 10, padding: 10 }}>
                 <Typography variant="title" color="inherit">
@@ -59,7 +70,7 @@ export class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {data.slice(this.state.page * this.state.rowsPerPage,
+                        {this.state.data.slice(this.state.page * this.state.rowsPerPage,
                             this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
                             .map(n => {
                                 return (
@@ -75,7 +86,7 @@ export class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
                 </Table>
                 <TablePagination
                     component="div"
-                    count={data.length}
+                    count={this.state.data.length}
                     labelRowsPerPage='Lignes par page'
                     rowsPerPage={this.state.rowsPerPage}
                     page={this.state.page}
@@ -109,6 +120,6 @@ export class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
     }
 
     private createData(entreprise: string, etudiant: string, statut: string, rowId: string) {
-        return { id: id++, entreprise, etudiant, statut, rowId };
+        return { entreprise, etudiant, statut, rowId };
     }
 }
